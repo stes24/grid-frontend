9aa878498c2d82f4e019c08dcc0a162c4c8f971a
stes24 <onafets24@gmail.com>
2026-05-27 16:33:21 +0200

    Prima commit (setup React/Vite)

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./index.css"
+import App from "./App.jsx"
+
+// createRoot affida la gestione del DOM dentro root a React, poi renderizza il componente
+// StrictMode viene ignorato in produzione (npm run build)
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
