dc6b384af88626b6814e9501383702d326cd031a
stes24 <onafets24@gmail.com>
2026-05-28 16:00:27 +0200

    Griglia statica, rivisti css

--- src/pages/Grid.jsx
+++ src/pages/Grid.jsx
@@ -1,17 +1,42 @@
 import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 import "./Grid.css"
 
+const NUM_ROWS = 10
+
+function Pixel({ id }) {
+  return (
+    <button className="pixel">{id}</button>
+  )
+}
+
 function Grid() {
+  // Hook di react-router-dom, ritorna funzione che fa navigare nel browser
+  const navigate = useNavigate()
+
   useEffect(() => {
     fetch("http://localhost:5000/pixels")
       .then((response) => response.json())
       .then((data) => console.log(data))
-  })
+  }, []) // L'array vuoto evita di farlo ripetere a ogni render
 
+  // La chiave dei pixel è i+1 a causa del serial nel db che parte da 1
   return (
-    <div className="grid">
-      <h1>Griglia</h1>
-      <p>La griglia verrà costruita qui.</p>
+    <div className="page">
+
+      <div className="grid">
+        {[...Array(NUM_ROWS).keys()].map(row => (
+          <div key={row} className="grid-row">
+            {[...Array(NUM_ROWS).keys()].map(column => {
+              const i = row * NUM_ROWS + column + 1
+              return <Pixel key={i} id={i} />
+            })}
+          </div>
+        ))}
+      </div>
+
+      <button onClick={() => navigate("/")}>Indietro</button>
+
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,11 @@
 import { useNavigate } from "react-router-dom"
-import "./Home.css"
 
 function Home() {
   // Hook di react-router-dom, ritorna funzione che fa navigare nel browser
   const navigate = useNavigate()
 
   return (
-    <div className="home">
+    <div className="page">
       <h1>Griglia collaborativa</h1>
       <button onClick={() => navigate("/grid")}>Gioca</button>
     </div>
