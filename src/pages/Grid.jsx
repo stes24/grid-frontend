353d93d018c6617380a9bd547d30dbf8f4e5a6c8
stes24 <onafets24@gmail.com>
2026-05-28 22:13:05 +0200

    Colore preso dal db, rivisti stati

--- src/pages/Grid.jsx
+++ src/pages/Grid.jsx
@@ -2,35 +2,53 @@ import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import "./Grid.css"
 
-function Pixel({ coords }) {
+function Pixel({ coords, color }) {
   return (
-    <button className="pixel">{coords}</button>
+    <button className="pixel" style={{ backgroundColor: color }}>{coords}</button>
   )
 }
 
 function Grid() {
   const navigate = useNavigate() // Hook di react-router-dom, ritorna funzione che fa navigare nel browser
-  const [pixels, setPixels] = useState([])
+  const [pixels, setPixels] = useState({})
+  const [gridSize, setGridSize] = useState({ numRows: 0, numCols: 0 })
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     fetch("http://localhost:5000/pixels")
       .then(response => response.json())
-      .then(data => setPixels(data))
+      .then(data => {
+        // Crea mappa che usa (r,c) come hash, contiene dati tipo 0,0: {color: "#FFFFFF", "pixel_col": 0, "pixel_row": 0}
+        const pixelMap = {}
+        data.forEach(p => {
+          pixelMap[computeHash(p.pixel_row, p.pixel_col)] = p
+        })
+
+        setGridSize({ // Ricava dimensioni della griglia dai valori massimi di riga e colonna nel db
+          numRows: data.length > 0 ? Math.max(...data.map(p => p.pixel_row)) + 1 : 0,
+          numCols: data.length > 0 ? Math.max(...data.map(p => p.pixel_col)) + 1 : 0
+        })
+        setPixels(pixelMap)
+        setLoading(false)
+      })
   }, []) // L'array vuoto evita di farlo ripetere a ogni render
 
-  // Ricava dimensioni della griglia dai valori massimi di riga e colonna nel db
-  const numRows = pixels.length > 0 ? Math.max(...pixels.map(p => p.pixel_row)) + 1 : 0
-  const numCols = pixels.length > 0 ? Math.max(...pixels.map(p => p.pixel_col)) + 1 : 0
+  // Ritorna subito se sta ancora caricando i dati
+  if (loading) {
+    return <div className="page"><p>Caricamento...</p></div>
+  }
 
   return (
     <div className="page">
 
       <div className="grid">
-        {[...Array(numRows).keys()].map(row => (
+        {[...Array(gridSize.numRows).keys()].map(row => (
           <div key={row} className="grid-row">
-            {[...Array(numCols).keys()].map(col =>
-              <Pixel key={`${row},${col}`} coords={`${row},${col}`} />
-            )}
+            {[...Array(gridSize.numCols).keys()].map(col => {
+              const coords = computeHash(row, col)
+              const color = pixels[coords]?.color
+              return <Pixel key={coords} coords={coords} color={color} />
+            })}
           </div>
         ))}
       </div>
@@ -39,6 +57,10 @@ function Grid() {
 
     </div>
   )
+}
+
+function computeHash(row, col) {
+  return `${row},${col}`
 }
 
 export default Grid
